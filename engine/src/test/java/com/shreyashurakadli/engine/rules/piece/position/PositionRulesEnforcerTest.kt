fa60fbfb601079f57cad750111bac8d6509e11f8
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-07-13 03:00:04 +0530

    test: added unit tests

--- engine/src/test/java/com/shreyashurakadli/engine/rules/piece/position/PositionRulesEnforcerTest.kt
+++ engine/src/test/java/com/shreyashurakadli/engine/rules/piece/position/PositionRulesEnforcerTest.kt
@@ -27,4 +27,104 @@ class PositionRulesEnforcerTest {
         )
         assertEquals(expectedPosition, actualPosition)
     }
+
+    @Test
+    fun updatePosition_enteringHomePosition() {
+        val state = Position(
+            part = 0,
+            tile = 3,
+            status = Status.Safe
+        )
+        val actualPosition = positionRulesEnforcer.updatePosition(
+            state = state,
+            diceValue = 4,
+            playerCount = 4
+        )
+        val expectedPosition = Position(
+            part = 0,
+            tile = 13,
+            status = Status.Safe
+        )
+        assertEquals(expectedPosition, actualPosition)
+    }
+
+    @Test
+    fun updatePosition_notEnteringHomePosition() {
+        val state = Position(
+            part = 0,
+            tile = 3,
+            status = Status.Safe
+        )
+        val actualPosition = positionRulesEnforcer.updatePosition(
+            state = state,
+            diceValue = 3,
+            playerCount = 4
+        )
+        val expectedPosition = Position(
+            part = 0,
+            tile = 6,
+            status = Status.Unsafe
+        )
+        assertEquals(expectedPosition, actualPosition)
+    }
+
+    @Test
+    fun updatePosition_EnteringFinishPositionFromLastNonHomePosition() {
+        val state = Position(
+            part = 0,
+            tile = 6,
+            status = Status.Unsafe
+        )
+        val actualPosition = positionRulesEnforcer.updatePosition(
+            state = state,
+            diceValue = 6,
+            playerCount = 4
+        )
+        val expectedPosition = Position(
+            part = 0,
+            tile = 18,
+            status = Status.Safe
+        )
+        assertEquals(expectedPosition, actualPosition)
+    }
+
+    @Test
+    fun updatePosition_EnteringFinishPositionFromHomePosition() {
+        val state = Position(
+            part = 0,
+            tile = 13,
+            status = Status.Safe
+        )
+        val actualPosition = positionRulesEnforcer.updatePosition(
+            state = state,
+            diceValue = 5,
+            playerCount = 4
+        )
+        val expectedPosition = Position(
+            part = 0,
+            tile = 18,
+            status = Status.Safe
+        )
+        assertEquals(expectedPosition, actualPosition)
+    }
+
+    @Test
+    fun updatePosition_EnteringAnotherPart() {
+        val state = Position(
+            part = 0,
+            tile = 12,
+            status = Status.Unsafe
+        )
+        val actualPosition = positionRulesEnforcer.updatePosition(
+            state = state,
+            diceValue = 1,
+            playerCount = 4
+        )
+        val expectedPosition = Position(
+            part = 1,
+            tile = 0,
+            status = Status.Unsafe
+        )
+        assertEquals(expectedPosition, actualPosition)
+    }
 }
