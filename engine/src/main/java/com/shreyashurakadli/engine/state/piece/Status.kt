21a10cd1cfda82bfed5a3269e37e584cf1a50c32
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-07-13 13:00:50 +0530

    chore: renamed InHome -> InBase

--- engine/src/main/java/com/shreyashurakadli/engine/state/piece/Status.kt
+++ engine/src/main/java/com/shreyashurakadli/engine/state/piece/Status.kt
@@ -1,7 +1,7 @@
 package com.shreyashurakadli.engine.state.piece
 
 internal interface Status {
-    object InHome : Status
+    object InBase : Status
     object OnBoard : Status
     object Finished : Status
 }
