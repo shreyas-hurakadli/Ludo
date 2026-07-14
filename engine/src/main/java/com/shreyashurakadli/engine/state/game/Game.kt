2d7b3bf847b5e9905659e5c8fbb31caf4e785810
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-07-14 21:36:16 +0530

    chore: changed the interpretation of current turn

--- engine/src/main/java/com/shreyashurakadli/engine/state/game/Game.kt
+++ engine/src/main/java/com/shreyashurakadli/engine/state/game/Game.kt
@@ -4,6 +4,6 @@ import com.shreyashurakadli.engine.state.player.Player
 
 internal data class Game(
     val players: List<Player>,
-    val currentTurn: Player,
-    val status: Status
+    val currentTurnPlayerIdx: Int,
+    val status: GameStatus
 )
