7d08e9932b36f9b44b3d3ab2037f8861cd2b241c
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-07-15 23:46:23 +0530

    chore: added id parameter

--- engine/src/main/java/com/shreyashurakadli/engine/state/piece/Piece.kt
+++ engine/src/main/java/com/shreyashurakadli/engine/state/piece/Piece.kt
@@ -3,6 +3,7 @@ package com.shreyashurakadli.engine.state.piece
 import com.shreyashurakadli.engine.state.piece.position.Position
 
 internal data class Piece(
+    val id: Int,
     val position: Position,
     val status: PieceStatus
 )
