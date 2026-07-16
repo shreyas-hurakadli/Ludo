b96f9f2d000143b0e3c60df6a3b5c110c3c5a4a9
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-07-17 02:00:44 +0530

    chore: separated normal position determination logic

---
+++ engine/src/main/java/com/shreyashurakadli/engine/rules/position/converter/PositionIsNormal.kt
@@ -0,0 +1,8 @@
+package com.shreyashurakadli.engine.rules.position.converter
+
+import com.shreyashurakadli.engine.constants.PositionConstants
+
+internal class PositionIsNormal {
+    operator fun invoke(absPos: Int, partCount: Int): Boolean =
+        absPos < partCount * PositionConstants.TOTAL_POSITIONS_IN_PART
+}
