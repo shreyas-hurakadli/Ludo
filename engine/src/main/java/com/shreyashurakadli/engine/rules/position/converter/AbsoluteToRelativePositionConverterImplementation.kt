1863259c4636b52f8b865410fa34a8f3e194b4d2
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-07-17 22:47:39 +0530

    fix: added base position handling

--- engine/src/main/java/com/shreyashurakadli/engine/rules/position/converter/AbsoluteToRelativePositionConverterImplementation.kt
+++ engine/src/main/java/com/shreyashurakadli/engine/rules/position/converter/AbsoluteToRelativePositionConverterImplementation.kt
@@ -2,36 +2,48 @@ package com.shreyashurakadli.engine.rules.position.converter
 
 import com.shreyashurakadli.engine.constants.PositionConstants
 import com.shreyashurakadli.engine.rules.position.common.DeterminePositionStatus
+import com.shreyashurakadli.engine.rules.position.common.PositionIsBase
 import com.shreyashurakadli.engine.rules.position.common.PositionIsNormal
 import com.shreyashurakadli.engine.state.position.Position
 import com.shreyashurakadli.engine.state.position.PositionStatus
+import kotlin.math.abs
 
 internal class AbsoluteToRelativePositionConverterImplementation(
     private val positionIsNormalPosition: PositionIsNormal,
-    private val determinePositionStatus: DeterminePositionStatus
+    private val determinePositionStatus: DeterminePositionStatus,
+    private val positionIsBase: PositionIsBase
 ) : AbsoluteToRelativePositionConverter {
     override fun calculateRelativePosition(position: Int, partCount: Int): Position {
-        val relativePosition = if (positionIsNormalPosition(position, partCount)) {
-            val tile = position % PositionConstants.TOTAL_POSITIONS_IN_PART
-            val part = (position - tile) / PositionConstants.TOTAL_POSITIONS_IN_PART
+        val relativePosition =
+            if (positionIsNormalPosition(position, partCount)) {
+                val tile = position % PositionConstants.TOTAL_POSITIONS_IN_PART
+                val part = (position - tile) / PositionConstants.TOTAL_POSITIONS_IN_PART
 
-            Position(
-                part = part,
-                tile = tile,
-                status = determinePositionStatus(tile)
-            )
-        } else {
-            val offset = position - partCount * PositionConstants.TOTAL_POSITIONS_IN_PART
+                Position(
+                    part = part,
+                    tile = tile,
+                    status = determinePositionStatus(tile)
+                )
+            } else if (positionIsBase(position)) {
+                Position(
+                    part = PositionConstants.START_PART,
+                    tile = abs(n = position) * PositionConstants.BASE_POSITION,
+                    status = PositionStatus.Safe
+                )
+            } else {
+                val offset = position - partCount * PositionConstants.TOTAL_POSITIONS_IN_PART
 
-            val tile = offset % PositionConstants.HOME_POSITIONS.size + PositionConstants.HOME_POSITION_0
-            val part = offset / PositionConstants.HOME_POSITIONS.size
+                // Home Positions + Final Position
+                val tile =
+                    offset % (PositionConstants.HOME_POSITIONS.size + 1) + PositionConstants.HOME_POSITION_0
+                val part = offset / (PositionConstants.HOME_POSITIONS.size + 1)
 
-            Position(
-                part = part,
-                tile = tile,
-                status = PositionStatus.Safe
-            )
-        }
+                Position(
+                    part = part,
+                    tile = tile,
+                    status = PositionStatus.Safe
+                )
+            }
 
         return relativePosition
     }
--- engine/src/main/java/com/shreyashurakadli/engine/rules/position/converter/RelativeToAbsolutePositionConverterImplementation.kt
+++ engine/src/main/java/com/shreyashurakadli/engine/rules/position/converter/RelativeToAbsolutePositionConverterImplementation.kt
@@ -1,11 +1,13 @@
 package com.shreyashurakadli.engine.rules.position.converter
 
 import com.shreyashurakadli.engine.constants.PositionConstants
+import com.shreyashurakadli.engine.rules.position.common.PositionIsBase
 import com.shreyashurakadli.engine.rules.position.common.PositionIsNormal
 import com.shreyashurakadli.engine.state.position.Position
 
 internal class RelativeToAbsolutePositionConverterImplementation(
-    val positionIsNormalPosition: PositionIsNormal
+    private val positionIsNormalPosition: PositionIsNormal,
+    private val positionIsBase: PositionIsBase
 ) : RelativeToAbsolutePositionConverter {
     override fun calculateAbsolutePosition(position: Position, partCount: Int): Int {
         val partOffset = position.part * PositionConstants.TOTAL_POSITIONS_IN_PART
@@ -17,10 +19,13 @@ internal class RelativeToAbsolutePositionConverterImplementation(
         // For normal position: absolutePosition == provisionalPosition
         val provisionalPosition = 2 * partOffset + position.tile
 
-        // Home position conversion and normal position conversion are handled separately
+        // Home position conversion, base position conversion and normal position
+        // conversion are handled separately
         val absolutePosition =
             if (positionIsNormalPosition(provisionalPosition, partCount)) {
                 provisionalPosition
+            } else if (positionIsBase(position)) {
+                position.tile * position.part
             } else {
                 val noOfHomePositionsInPart = PositionConstants.HOME_POSITIONS.size
                 val homePositionOffset = noOfHomePositionsInPart * position.part
