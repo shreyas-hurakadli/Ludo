b7de9dd1f42d639792e4e3c5c7a750452fc22155
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-07-19 12:36:47 +0530

    fix: corrected HasCapturedPieceLogic

--- engine/src/main/java/com/shreyashurakadli/engine/rules/piece/common/HasCapturedPiece.kt
+++ engine/src/main/java/com/shreyashurakadli/engine/rules/piece/common/HasCapturedPiece.kt
@@ -1,14 +1,30 @@
 package com.shreyashurakadli.engine.rules.piece.common
 
+import com.shreyashurakadli.engine.rules.position.converter.RelativeToAbsolutePositionConverter
 import com.shreyashurakadli.engine.state.piece.Piece
 import com.shreyashurakadli.engine.state.player.Player
 import com.shreyashurakadli.engine.state.position.PositionStatus
 
-internal class HasCapturedPiece {
-    operator fun invoke(players: List<Player>, playerId: Int, piece: Piece): Boolean {
+internal class HasCapturedPiece(
+    private val converter: RelativeToAbsolutePositionConverter
+) {
+    operator fun invoke(
+        players: List<Player>,
+        playerId: Int,
+        piece: Piece,
+        partCount: Int
+    ): Boolean {
         if (piece.position.status == PositionStatus.Safe) {
             return false
         }
+
+        val capturer = players.find { it.id == playerId } ?: return false
+
+        val absolutePosition = converter.calculateAbsolutePosition(
+            position = piece.position,
+            partCount = partCount,
+            playerPart = capturer.id
+        )
 
         for (player in players) {
             if (player.id == playerId) {
@@ -16,12 +32,17 @@ internal class HasCapturedPiece {
             }
 
             for (otherPiece in player.pieces) {
-                if (otherPiece.position == piece.position) {
+                val otherPieceAbsolutePosition = converter.calculateAbsolutePosition(
+                    position = otherPiece.position,
+                    partCount = partCount,
+                    playerPart = player.id
+                )
+                if (otherPieceAbsolutePosition == absolutePosition) {
                     return true
                 }
             }
-        }
 
+        }
         return false
     }
 }
