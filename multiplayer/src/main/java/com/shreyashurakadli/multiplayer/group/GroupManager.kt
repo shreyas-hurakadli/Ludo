aec1e199d2e0e1bdeef784cd6908cecb49716306
shreyas-hurakadli <shreyashurakadli23@gmail.com>
2026-08-23 15:22:54 +0530

    feat: added GroupManager

---
+++ multiplayer/src/main/java/com/shreyashurakadli/multiplayer/group/GroupManager.kt
@@ -0,0 +1,38 @@
+package com.shreyashurakadli.multiplayer.group
+
+import com.shreyashurakadli.multiplayer.transport.Transport
+
+internal class GroupManager(
+    val transport: Transport
+) {
+    /**
+     * List of peers in the group
+     */
+    val group = transport.connectedEndpoints
+
+    /**
+     * Checks if the required resources are available and required permissions are granted.
+     * @param name Name that represents local node to other peers
+     * @param token Room code
+     */
+    fun checkPrerequisites(name: String, token: String) {
+        transport.register(localName = name, clusterToken = token)
+        transport.checkPrerequisites()
+    }
+
+    /**
+     * Adds a peer to the group
+     * @param peer Peer ID
+     */
+    fun addPeer(peer: String) {
+        transport.connect(peer)
+    }
+
+    /**
+     * Removes a peer from the group
+     * @param peer Peer ID
+     */
+    fun removePeer(peer: String) {
+        transport.disconnect(peer)
+    }
+}
